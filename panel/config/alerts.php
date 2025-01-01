dced4bac4d254ca205f6c66661ad25a3b7f5bb97
Hossein Fadaei <92361081+ARASHFADAEE@users.noreply.github.com>
2025-01-02 01:07:23 +0330

    add category action fix

--- panel/config/alerts.php
+++ panel/config/alerts.php
@@ -99,3 +99,10 @@
         <p class="text-sm text-red-800  dark:bg-gray-800 dark:text-red-400 "><?= $_GET['message']?></p>
     </div>
 <?php endif;?>
+
+
+<?php if(isset($_GET['success']) && $_GET['success']=='ok'):?>
+    <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+        <p><?= $_GET['message']?></p>
+    </div>
+<?php endif;?>
